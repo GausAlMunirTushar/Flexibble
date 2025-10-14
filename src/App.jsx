847c705745a149a20fb1171ac8e1f7ee630462ae
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-10-14 23:23:00 +0600

    feat: footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
 import Portfolio from "./components/Portfolio";
-
+import Footer from "./components/Footer";
 function App() {
 	return (
 		<>
 			<Header />
 			<Portfolio />
+			<Footer />
 		</>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,69 @@
+import Logo from "./Logo";
+
+const Footer = () => {
+	return (
+		<footer className="bg-gray-50">
+			<section className="w-7xl mx-auto flex gap-10 py-14 px-4 ">
+				<section className="w-3/12">
+					<Logo />
+					<p className="mt-6">
+						Flexibbble is the world's leading community for
+						creatives to share, grow, and get hired.
+					</p>
+				</section>
+				<section className="w-9/12 flex justify-between">
+					<div>
+						<ul className="py-2">
+							<li className="font-bold">For Designers</li>
+							<li>Go Pro !</li>
+							<li>Explore design work</li>
+							<li>Design blog</li>
+							<li>Overtime podcast</li>
+							<li>Playoffs</li>
+							<li>Refer a Friend</li>
+							<li>Code of Conduct</li>
+						</ul>
+					</div>
+					<div>
+						<ul className="py-2">
+							<li className="font-bold">For Designers</li>
+							<li>Go Pro !</li>
+							<li>Explore design work</li>
+							<li>Design blog</li>
+							<li>Overtime podcast</li>
+							<li>Playoffs</li>
+							<li>Refer a Friend</li>
+							<li>Code of Conduct</li>
+						</ul>
+					</div>
+					<div>
+						<ul className="py-2">
+							<li className="font-bold">For Designers</li>
+							<li>Go Pro !</li>
+							<li>Explore design work</li>
+							<li>Design blog</li>
+							<li>Overtime podcast</li>
+							<li>Playoffs</li>
+							<li>Refer a Friend</li>
+							<li>Code of Conduct</li>
+						</ul>
+					</div>
+					<div>
+						<ul className="py-2">
+							<li className="font-bold">For Designers</li>
+							<li>Go Pro !</li>
+							<li>Explore design work</li>
+							<li>Design blog</li>
+							<li>Overtime podcast</li>
+							<li>Playoffs</li>
+							<li>Refer a Friend</li>
+							<li>Code of Conduct</li>
+						</ul>
+					</div>
+				</section>
+			</section>
+		</footer>
+	);
+};
+
+export default Footer;
